6b7acb74f7167ebe9303d2715d1d5913d8ce4f71
Corbitt O'Connor <Corbitt@Corbitt-OConnors-MacBook-Pro-2.local>
2015-01-21 18:22:33 -0500

    Initial Commit

---
+++ QuaffOn/DataViewController.h
@@ -0,0 +1,17 @@
+//
+//  DataViewController.h
+//  QuaffOn
+//
+//  Created by Corbitt O'Connor on 1/21/15.
+//  Copyright (c) 2015 Corbitt O'Connor. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DataViewController : UIViewController
+
+@property (strong, nonatomic) IBOutlet UILabel *dataLabel;
+@property (strong, nonatomic) id dataObject;
+
+@end
+
